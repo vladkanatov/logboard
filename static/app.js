b2466d095a97709218f3697ab0aa3443b7bdd856
votanak <v0tanak@yandex.ru>
2024-11-21 02:55:08 +0300

    21.11-2

--- static/app.js
+++ static/app.js
@@ -2,19 +2,18 @@ let currentTab = 'packages-common';
 let searchText = { 'packages-common': '', eap: '', sdk: '' };
 const logDisplay = document.getElementById('log-display');
 const searchInput = document.getElementById('search-input');
-searchInput.value = 'sasd';
+searchInput.value = '';
 
 const ws = new WebSocket('ws://localhost:8000/logs?tab=packages-common');
 
 // Устанавливает текущую вкладку и загружает данные для нее
 function setCurrentTab(tab) {
+  let logs = document.getElementsByClassName('log');
+  logDisplay.replaceChildren();
+  searchText[currentTab] = searchInput.value;
   currentTab = tab;
   searchInput.value = searchText[currentTab];
 }
-searchInput.addEventListener('input', () => {
-  searchText[currentTab] = searchInput.value;
-  console.log(searchText, currentTab);
-});
 
 const chgSortDirHandler = () => {
   logDisplay.classList.contains('reversed')
@@ -32,7 +31,7 @@ ws.onmessage = function (line) {
   const logLine = document.createElement('div');
   if (line.data.startsWith('success:')) {
     logLine.classList.add('success', 'log');
-    line = line.slice(8).trim(); // Убираем "success:" из вывода и пробелы
+    line = line.data.slice(8).trim(); // Убираем "success:" из вывода и пробелы
   } else if (line.data.startsWith('error:')) {
     logLine.classList.add('error', 'log');
     line = line.data.slice(6).trim(); // Убираем "error:" из вывода и пробелы
@@ -41,7 +40,7 @@ ws.onmessage = function (line) {
     line = line.data.slice(5).trim(); // Убираем "info:" из вывода и пробелы
   } else {
     line = line.data;
-    logLine.classList.add('date-divider');
+    logLine.classList.add('date-divider', 'log');
   }
 
   // Обработка Markdown-ссылок в формате [текст](ссылка)
@@ -53,3 +52,19 @@ ws.onmessage = function (line) {
 
   logDisplay.appendChild(logLine);
 };
+
+const searching = (event) => {
+  // searchText[currentTab] = searchInput.value;
+  let logs = document.getElementsByClassName('log');
+  if (searchInput.value.length < 2) {
+    for (let log of logs) log.style.display = 'block';
+  } else {
+    for (let log of logs) {
+      log.textContent.toLowerCase().includes(event.target.value.toLowerCase())
+        ? (log.style.display = 'block')
+        : (log.style.display = 'none');
+    }
+  }
+};
+
+searchInput.addEventListener('input', searching);
