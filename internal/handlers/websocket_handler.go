8c4b3bf87666de82dcb8a1b9fb79567e5096421c
votanak <v0tanak@yandex.ru>
2024-11-21 22:55:35 +0300

    21.11-6

--- internal/handlers/websocket_handler.go
+++ internal/handlers/websocket_handler.go
@@ -52,9 +52,9 @@ func HandleWebSocketLogs(w http.ResponseWriter, r *http.Request) {
 	log.Printf("New WebSocket client connected for tab: %s", tab)
 
 	// Отправляем содержимое файла логов при подключении
-	// if err := sendLogFile(tab, conn); err != nil {
-	// 	log.Printf("Error sending log file: %v", err)
-	// }
+	if err := sendLogFile(tab, conn); err != nil {
+		log.Printf("Error sending log file: %v", err)
+	}
 
 	// Постоянно отправляем новые строки из файла (tailing)
 	if err := tailLogFile(tab, conn); err != nil {
